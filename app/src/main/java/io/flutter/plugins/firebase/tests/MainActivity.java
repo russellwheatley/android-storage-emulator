6f5b85d5c5bda32fc05d08e196ff15e9480658e4
russellwheatley <russellwheatley85@gmail.com>
2024-01-23 11:21:34 +0000

    firestore

--- app/src/main/java/io/flutter/plugins/firebase/tests/MainActivity.java
+++ app/src/main/java/io/flutter/plugins/firebase/tests/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -39,22 +40,32 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+                storage.useEmulator("10.0.2.2", 9199);
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseApp app = FirebaseApp.getInstance();
-//                FirebaseStorage storage = FirebaseStorage.getInstance(app, "gs://flutterfire-e2e-tests-two");
-                FirebaseStorage storage = FirebaseStorage.getInstance();
-                storage.useEmulator("10.0.2.2", 9199);
+                FirebaseStorage storage = FirebaseStorage.getInstance(app, "gs://flutterfire-e2e-tests-two");
+                FirebaseFirestore firestore = FirebaseFirestore.getInstance(app);
                 Log.d(TAG, "onClick: ");
-                storage.getReference().child("flutter-tests").child("foo.txt").putBytes(new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}).addOnCompleteListener(task -> {
+
+                firestore.collection("flutter-tests").whereArrayContains("users", null).get().addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
                         Log.d(TAG, "Success");
+                        Log.d(TAG, "onClick: " + task.getResult().getDocuments().size());
                     } else {
                         Log.d(TAG, "Failure");
                     }
                 });
+//                storage.getReference().child("flutter-tests").child("bar.txt").putBytes(new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}).addOnCompleteListener(task -> {
+//                    if (task.isSuccessful()) {
+//                        Log.d(TAG, "Success");
+//                    } else {
+//                        Log.d(TAG, "Failure");
+//                    }
+//                });
             }
         });
     }
