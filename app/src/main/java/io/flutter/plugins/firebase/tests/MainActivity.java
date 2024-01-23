475906360720f37b8463e3feffe349325201e765
russellwheatley <russellwheatley85@gmail.com>
2024-01-23 15:31:36 +0000

    firestore

--- app/src/main/java/io/flutter/plugins/firebase/tests/MainActivity.java
+++ app/src/main/java/io/flutter/plugins/firebase/tests/MainActivity.java
@@ -1,12 +1,32 @@
 package io.flutter.plugins.firebase.tests;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.android.gms.auth.api.identity.Identity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
@@ -23,10 +43,16 @@ import io.flutter.plugins.firebase.tests.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "YYYYYYYYY";
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
+    private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
+
+    private SignInClient oneTapClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,34 +66,98 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-                FirebaseStorage storage = FirebaseStorage.getInstance();
-                storage.useEmulator("10.0.2.2", 9199);
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseApp app = FirebaseApp.getInstance();
-                FirebaseStorage storage = FirebaseStorage.getInstance(app, "gs://flutterfire-e2e-tests-two");
-                FirebaseFirestore firestore = FirebaseFirestore.getInstance(app);
-                Log.d(TAG, "onClick: ");
+                mAuth = FirebaseAuth.getInstance(app);
+                oneTapClient = Identity.getSignInClient(binding.getRoot().getContext());
+                BeginSignInRequest signUpRequest = BeginSignInRequest.builder()
+                        .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                                .setSupported(true)
+                                // Your server's client ID, not your Android client ID.
+                                .setServerClientId(getString(R.string.default_web_client_id))
+                                // Only show accounts previously used to sign in.
+                                .setFilterByAuthorizedAccounts(true)
+                                .build())
+                        .build();
 
-                firestore.collection("flutter-tests").whereArrayContains("users", null).get().addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        Log.d(TAG, "Success");
-                        Log.d(TAG, "onClick: " + task.getResult().getDocuments().size());
-                    } else {
-                        Log.d(TAG, "Failure");
-                    }
-                });
-//                storage.getReference().child("flutter-tests").child("bar.txt").putBytes(new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}).addOnCompleteListener(task -> {
-//                    if (task.isSuccessful()) {
-//                        Log.d(TAG, "Success");
-//                    } else {
-//                        Log.d(TAG, "Failure");
-//                    }
-//                });
+                oneTapClient.beginSignIn(signUpRequest)
+                        .addOnSuccessListener(new OnSuccessListener<BeginSignInResult>() {
+                            @Override
+                            public void onSuccess(BeginSignInResult result) {
+                                try {
+                                    startIntentSenderForResult(
+                                            result.getPendingIntent().getIntentSender(), REQ_ONE_TAP,
+                                            null, 0, 0, 0);
+                                } catch (IntentSender.SendIntentException e) {
+                                    Log.e(TAG, "Couldn't start One Tap UI: " + e.getLocalizedMessage());
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // No saved credentials found. Launch the One Tap sign-up flow, or
+                                // do nothing and continue presenting the signed-out UI.
+                                Log.d(TAG, "onFailure " + Objects.requireNonNull(e.getLocalizedMessage()));
+                            }
+                        });
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: " + requestCode);
+        if (requestCode == REQ_ONE_TAP) {
+            try {
+                SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
+                String idToken = credential.getGoogleIdToken();
+                if (idToken != null) {
+                    // Got an ID token from Google. Use it to authenticate
+                    // with Firebase.
+                    AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+                    mAuth.signInWithCredential(firebaseCredential)
+                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithCredential:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+
+                                        AuthCredential credential = EmailAuthProvider.getCredential("russellwheatley85@gmail.com", "qwerty123");
+
+                                        assert user != null;
+                                        user.linkWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                                if (task.isSuccessful()) {
+                                                    Log.d(TAG, "onComplete: LINKED");
+                                                } else {
+                                                    Log.d(TAG, "onComplete: NOT LINKED");
+                                                    Log.d(TAG, "onComplete: " + Objects.requireNonNull(task.getException()).getMessage());
+                                                }
+                                            }
+                                        });
+
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "signInWithCredential:failure", task.getException());
+
+                                    }
+                                }
+                            });
+                } else {
+                    Log.d(TAG, "onActivityResult: NNNNNNNNNNNNNNNNNN");
+                }
+            } catch (ApiException e) {
+                // ...
+            }
+        }
     }
 
     @Override
